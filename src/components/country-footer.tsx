216a033ae6d7e3a695eb6c01e8922f1b8a66daed
numdefined <contact@donation.watch>
2026-02-17 12:01:18 +0100

    fix: move social links to page footer to avoid it only appearing on country pages

--- src/components/country-footer.tsx
+++ src/components/country-footer.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 
 import { PageLogo } from "./page-logo";
 import { t } from "../app/[locale]/translations";
-import { BSKY_URL, GITHUB_URL, TWITTER_URL } from "../utils/config";
 import { formatDate } from "../utils/formatter";
 import { getBuild } from "../utils/loader/build";
 
@@ -23,89 +22,25 @@ export const CountryFooter = ({
 }) => {
   return (
     <section className="container mx-auto shrink-0 px-4 text-gray-600 dark:text-gray-400">
-      <div className="grid grid-cols-2 items-start justify-between py-4 sm:grid-cols-3 sm:justify-items-center">
-        <div className="col-span-2 sm:col-span-1 sm:justify-self-start">
-          <div className="flex justify-between sm:block">
-            <Link
-              prefetch={false}
-              href={`/${locale}/${country.id}`}
-              className="group hover:text-primary-700 dark:hover:text-primary-400 flex items-center space-x-1 p-2 dark:text-white"
-            >
-              <PageLogo size={16} />
-              <div className="text-sm font-semibold">DonationWatch</div>
-            </Link>
-            <ul className="mt-2">
-              <li className="inline-block">
-                <a
-                  className="hover:text-primary-800 dark:hover:text-primary-400 inline-block p-2 text-gray-500"
-                  target="_blank"
-                  title="Bluesky"
-                  href={BSKY_URL}
-                  rel="noreferrer"
-                >
-                  <svg
-                    width="16px"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M12 10.8c-1-2.1-4-6-6.8-8C2.6 1 1.6 1.3.9 1.6.1 1.9 0 3 0 3.8c0 .7.4 5.6.6 6.4C1.4 13 4.3 14 7 13.6c-4 .6-7.4 2-2.8 7 5 5.3 6.8-1 7.8-4.2 1 3.2 2 9.3 7.7 4.3 4.3-4.3 1.2-6.5-2.7-7a8.7 8.7 0 0 1-.4-.1h.4c2.7.3 5.6-.6 6.4-3.4.2-.8.6-5.7.6-6.4 0-.7-.1-1.9-.9-2.2-.7-.3-1.7-.7-4.3 1.2-2.8 2-5.7 5.9-6.8 8Z"
-                    />
-                  </svg>
-                </a>
-              </li>
-              <li className="inline-block">
-                <a
-                  className="hover:text-primary-800 dark:hover:text-primary-400 inline-block p-2 text-gray-500"
-                  target="_blank"
-                  title="Twitter"
-                  href={TWITTER_URL}
-                  rel="noreferrer"
-                >
-                  <svg
-                    width="16px"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M18.9 1.15h3.68l-8.04 9.2L24 22.84h-7.4l-5.8-7.59-6.64 7.59H.47l8.6-9.83L0 1.15h7.6l5.24 6.94Zm-1.29 19.5h2.04L6.49 3.23h-2.2Z"
-                    />
-                  </svg>
-                </a>
-              </li>
-              <li className="inline-block">
-                <a
-                  className="hover:text-primary-800 dark:hover:text-primary-400 inline-block p-2 text-gray-500"
-                  target="_blank"
-                  title="GitHub"
-                  href={GITHUB_URL}
-                  rel="noreferrer"
-                >
-                  <svg
-                    width="16px"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1.1-.8.1-.7.1-.7 1.2.1 1.9 1.2 1.9 1.2 1.1 1.8 2.8 1.3 3.5 1 .1-.8.4-1.3.8-1.6-2.7-.3-5.5-1.3-5.5-5.9 0-1.3.5-2.4 1.2-3.2-.1-.3-.5-1.5.1-3.2 0 0 1-.3 3.3 1.2a11.5 11.5 0 0 1 6 0C17.3 4.7 18.3 5 18.3 5c.7 1.7.2 2.9.1 3.2.8.8 1.2 1.9 1.2 3.2 0 4.6-2.8 5.6-5.5 5.9.4.4.8 1.1.8 2.2v3.3c0 .3.2.7.8.6A12 12 0 0 0 12 .3"
-                    />
-                  </svg>
-                </a>
-              </li>
-            </ul>
-          </div>
+      <div className="grid-cols-3 items-start justify-between py-2 sm:grid sm:justify-items-center">
+        <div className="sm:justify-self-start">
+          <Link
+            prefetch={false}
+            href={`/${locale}/${country.id}`}
+            className="group hover:text-primary-700 dark:hover:text-primary-400 flex items-center space-x-1 p-2 dark:text-white"
+          >
+            <PageLogo size={16} />
+            <div className="text-sm font-semibold">DonationWatch</div>
+          </Link>
         </div>
 
-        <div className="p-2 text-sm">
+        <div className="px-2 text-sm sm:p-2">
           {t(translations.footer.build, {
             date: formatDate(locale, new Date(getBuild(country.id).t)),
           })}
         </div>
 
-        <div className="flex flex-col items-end justify-self-end">
+        <div className="flex sm:justify-self-end">
           <a
             className="hover:text-primary-800 dark:hover:text-primary-400 p-2 text-sm"
             target="_blank"
--- src/components/page-footer.tsx
+++ src/components/page-footer.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 
 import { Translation } from "./translation";
-import { GITHUB_URL } from "../utils/config";
+import { BSKY_URL, GITHUB_URL, TWITTER_URL } from "../utils/config";
 
 import type { Translations } from "../messages/translations";
 import type { ConstLocale } from "../utils/locales";
@@ -19,8 +19,8 @@ export const PageFooter = async ({
 }) => {
   return (
     <footer className="container mx-auto shrink-0 px-4 text-gray-600 dark:text-gray-400">
-      <div className="flex flex-col justify-between gap-2 py-4 text-sm sm:flex-row">
-        <div>
+      <div className="flex grid-cols-3 flex-col gap-2 py-4 text-sm sm:grid sm:flex-row">
+        <div className="sm:justify-self-start">
           <span aria-label={translations.copyright}>&copy;</span> {currentYear}{" "}
           DonationWatch
           <div className="mt-1 text-xs">
@@ -42,7 +42,70 @@ export const PageFooter = async ({
           </div>
         </div>
 
-        <div className="flex flex-wrap space-x-2">
+        <ul className="flex items-center sm:justify-center">
+          <li className="inline-block">
+            <a
+              className="hover:text-primary-800 dark:hover:text-primary-400 inline-block p-2 text-gray-500"
+              target="_blank"
+              title="Bluesky"
+              href={BSKY_URL}
+              rel="noreferrer"
+            >
+              <svg
+                width="16px"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12 10.8c-1-2.1-4-6-6.8-8C2.6 1 1.6 1.3.9 1.6.1 1.9 0 3 0 3.8c0 .7.4 5.6.6 6.4C1.4 13 4.3 14 7 13.6c-4 .6-7.4 2-2.8 7 5 5.3 6.8-1 7.8-4.2 1 3.2 2 9.3 7.7 4.3 4.3-4.3 1.2-6.5-2.7-7a8.7 8.7 0 0 1-.4-.1h.4c2.7.3 5.6-.6 6.4-3.4.2-.8.6-5.7.6-6.4 0-.7-.1-1.9-.9-2.2-.7-.3-1.7-.7-4.3 1.2-2.8 2-5.7 5.9-6.8 8Z"
+                />
+              </svg>
+            </a>
+          </li>
+          <li className="inline-block">
+            <a
+              className="hover:text-primary-800 dark:hover:text-primary-400 inline-block p-2 text-gray-500"
+              target="_blank"
+              title="Twitter"
+              href={TWITTER_URL}
+              rel="noreferrer"
+            >
+              <svg
+                width="16px"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill="currentColor"
+                  d="M18.9 1.15h3.68l-8.04 9.2L24 22.84h-7.4l-5.8-7.59-6.64 7.59H.47l8.6-9.83L0 1.15h7.6l5.24 6.94Zm-1.29 19.5h2.04L6.49 3.23h-2.2Z"
+                />
+              </svg>
+            </a>
+          </li>
+          <li className="inline-block">
+            <a
+              className="hover:text-primary-800 dark:hover:text-primary-400 inline-block p-2 text-gray-500"
+              target="_blank"
+              title="GitHub"
+              href={GITHUB_URL}
+              rel="noreferrer"
+            >
+              <svg
+                width="16px"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1.1-.8.1-.7.1-.7 1.2.1 1.9 1.2 1.9 1.2 1.1 1.8 2.8 1.3 3.5 1 .1-.8.4-1.3.8-1.6-2.7-.3-5.5-1.3-5.5-5.9 0-1.3.5-2.4 1.2-3.2-.1-.3-.5-1.5.1-3.2 0 0 1-.3 3.3 1.2a11.5 11.5 0 0 1 6 0C17.3 4.7 18.3 5 18.3 5c.7 1.7.2 2.9.1 3.2.8.8 1.2 1.9 1.2 3.2 0 4.6-2.8 5.6-5.5 5.9.4.4.8 1.1.8 2.2v3.3c0 .3.2.7.8.6A12 12 0 0 0 12 .3"
+                />
+              </svg>
+            </a>
+          </li>
+        </ul>
+
+        <div className="flex flex-wrap items-center gap-2 sm:justify-end">
           <Link
             prefetch={false}
             className="hover:text-primary-800 dark:hover:text-primary-400 block"
@@ -78,14 +141,6 @@ export const PageFooter = async ({
           >
             {translations.about.title}
           </Link>
-          <a
-            className="hover:text-primary-800 dark:hover:text-primary-400 block"
-            href={GITHUB_URL}
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>
         </div>
       </div>
     </footer>
