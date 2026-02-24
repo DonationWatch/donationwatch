349441ed41317ee66e18f158c7ce216aef82cb55
numdefined <contact@donation.watch>
2026-02-24 18:55:45 +0100

    chore: don't add wiki, transparency imports to country-data-loaders because they're bundled

--- src/utils/loader/country-data-loaders.ts
+++ src/utils/loader/country-data-loaders.ts
@@ -7,9 +7,6 @@ import type { HistoryEntry } from "../data/get-history";
 type DefaultExport<T> = { default: T };
 
 interface CountryLoaders {
-  wiki: (
-    country: Country,
-  ) => Promise<DefaultExport<{ articles: Record<number, string> }>>;
   partySums: (country: Country) => Promise<DefaultExport<PartyYearsSums>>;
   mostRecent: (country: Country) => Promise<DefaultExport<HistoryEntry[]>>;
   biggestDonors: (country: Country) => Promise<DefaultExport<BigDonor[]>>;
@@ -19,27 +16,16 @@ interface CountryLoaders {
       parties: Party[];
     }>
   >;
-  transparency: (country: Country) => Promise<
-    DefaultExport<{
-      filteredDonors: string[];
-      normalizedDonors: Record<string, string[]>;
-    }>
-  >;
   biggestDonations: (country: Country) => Promise<DefaultExport<Donation[]>>;
 }
 
-import(`../../data/australia/config`);
-
+// Note: anything that's imported here will be bundled in the server bundle
 const loaders: CountryLoaders = {
-  wiki: (country: Country) =>
-    import(`../../data/${country}/wikipedia-articles`),
   partySums: (country: Country) => import(`../../data/${country}/party-sums`),
   mostRecent: (country: Country) => import(`../../data/${country}/most-recent`),
   biggestDonors: (country: Country) =>
     import(`../../data/${country}/biggest-donors`),
   yearParties: (country: Country) => import(`../../data/${country}/config`),
-  transparency: (country: Country) =>
-    import(`../../data/${country}/transparency`),
   biggestDonations: (country: Country) =>
     import(`../../data/${country}/biggest-donations`),
 };
--- src/utils/loader/wiki.ts
+++ src/utils/loader/wiki.ts
@@ -1,8 +1,8 @@
-import { loadCountryData } from "./country-data-loaders";
-
 import type { Country } from "../countries";
 
 export const getWikiArticles = (
   country: Country,
 ): Promise<{ articles: Record<number, string> }> =>
-  loadCountryData(country, "wiki");
+  import(`../../data/${country}/wikipedia-articles`).then(
+    (module) => module.default,
+  );
