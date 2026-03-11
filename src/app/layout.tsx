0e902a270e1e0779c2e658f8b40ea48a3eeef396
numdefined <contact@donation.watch>
2026-03-11 12:01:52 +0100

    chore: add dark mode favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,32 @@
 import "./globals.css";
-import { BASE_URL } from "../utils/config";
-import { baseOpenGraph, baseTwitter } from "../utils/meta";
 
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
+
+import { BASE_URL } from "@/utils/config";
+import { baseOpenGraph, baseTwitter } from "@/utils/meta";
 
 export const metadata: Metadata = {
   title: "DonationWatch",
   metadataBase: new URL(BASE_URL),
   openGraph: baseOpenGraph(),
   twitter: baseTwitter(),
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        url: "/favicon.ico",
+        media: "(prefers-color-scheme: light)",
+      },
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        url: "/favicon-dark.svg",
+        media: "(prefers-color-scheme: dark)",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
