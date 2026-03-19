38184be142c60d66ca54897824f740c74a6aa3f8
numdefined <contact@donation.watch>
2026-03-19 16:41:19 +0100

    fix: fix CLS when loading dynamic multiple party gradient

--- src/components/dynamic-stacked-party-line.tsx
+++ src/components/dynamic-stacked-party-line.tsx
@@ -16,5 +16,14 @@ export const DynamicAbsoluteMultiplePartySumsGradient = dynamic(
     import("./stacked-party-line").then(
       (mod) => mod.AbsoluteMultiplePartySumsGradient,
     ),
-  { ssr: false },
+  {
+    ssr: false,
+    loading: () => (
+      <>
+        <div className="h-full w-full rounded-xs bg-gray-200 dark:bg-gray-700"></div>
+        {/*offset for page head on mobile */}
+        <div className="mb-8 lg:mb-0"></div>
+      </>
+    ),
+  },
 );
