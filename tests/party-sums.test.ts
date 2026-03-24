f002a79bc2ae2eda100ca0305c3ddd851c637433
numdefined <contact@donation.watch>
2026-03-24 17:03:46 +0100

    test(nl): update party sums test to most recent nl dataset

--- tests/party-sums.test.ts
+++ tests/party-sums.test.ts
@@ -130,7 +130,7 @@ describe("NL", () => {
     const data = nlSums["2026"];
 
     hasExpectedDonations(data, [
-      ["D66", 210_090.0],
+      ["D66", 260_090.0],
       ["VVD", 190_000.0],
       ["GL", 10_000],
       ["PVDD", 200_000.0],
