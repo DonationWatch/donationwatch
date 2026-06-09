0815e65f62f5b3aac9f277805e4c0af3447c6e4c
numdefined <contact@donation.watch>
2026-06-09 15:14:49 +0200

    chore(eu): update 2026 foundations url

--- tasks/load-data/eu/eu-loader.ts
+++ tasks/load-data/eu/eu-loader.ts
@@ -749,7 +749,7 @@ export class EuLoader extends DataLoader {
       parties:
         "https://www.appf.europa.eu/cmsdata/307481/2026%20PARTIES%20Donations%20table%20as%20of%202026-06-03.xlsx",
       foundations:
-        "https://www.appf.europa.eu/cmsdata/307107/2026%20FOUNDATIONS%20Donations%20table%20as%20of%202026-05-28.xlsx",
+        "https://www.appf.europa.eu/cmsdata/307638/2026%20FOUNDATIONS%20Donations%20table%20as%20of%202026-06-05.xlsx",
     },
   };
 
