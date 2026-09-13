72868127fb985156484a5e28b84aa55ec2daaa13
numdefined <contact@donation.watch>
2026-09-13 10:48:50 +0200

    chore(nl): update 2026 document url

--- tasks/load-data/nl/nl-loader.ts
+++ tasks/load-data/nl/nl-loader.ts
@@ -205,6 +205,13 @@ export class NlLoader extends DataLoader {
       color: "#0b0a37",
       wiki: 74635762,
     },
+    "Progressief Nederland": {
+      name: "Progressief Nederland",
+      short: "PRO",
+      code: "PRO",
+      color: "#ff0000",
+      wiki: 82789412,
+    },
   };
 
   constructor() {
@@ -526,7 +533,7 @@ export class NlLoader extends DataLoader {
   };
 
   yearFiles: Record<string, string> = {
-    2026: "https://www.rijksoverheid.nl/site/binaries/site-content/collections/documents/2026/01/21/overzicht-substantiele-giften-aan-politieke-partijen-2026/overzicht-substantiele-giften-aan-politieke-partijen-2026-versie-16-juni-2026.ods",
+    2026: "https://www.rijksoverheid.nl/site/binaries/site-content/collections/documents/2026/01/21/overzicht-substantiele-giften-aan-politieke-partijen-2026/overzicht-substantiele-giften-aan-politieke-partijen-2026-versie-8-september-2026.ods",
     2025: "https://www.rijksoverheid.nl/site/binaries/site-content/collections/documents/2025/01/31/overzicht-substantiele-giften-aan-politieke-partijen-2025/overzicht-substantiele-giften-aan-politieke-partijen-2025-versie-29-06-2026.ods",
     2024: "https://www.rijksoverheid.nl/site/binaries/site-content/collections/documents/2025/10/22/overzicht-van-giften-aan-politieke-partijen-en-hun-neveninstellingen-in-2024/overzicht-giften-aan-politieke-partijen-2024.ods",
     2023: "https://www.rijksoverheid.nl/site/binaries/site-content/collections/documents/2024/10/22/overzicht-giften-aan-politieke-partijen-2023/1+Overzicht+giften+2023.ods",
